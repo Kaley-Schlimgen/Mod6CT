ad42e5eef1d44286f6b608f42393ce85c3423bf3
Kaley-Schlimgen <kaleynicole89@gmail.com>
2024-12-26 20:01:06 -0700

    final

--- src/Student.java
+++ src/Student.java
@@ -4,12 +4,10 @@ public class Student {
     String name;
     String address;
 
-
     public Student(int rollNo, String name, String address) {
         this.rollNo = rollNo;
         this.name = name;
         this.address = address;
-
     }
 
     @Override
